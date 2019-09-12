b9839ccad5850bdf5fd4e575fc47e6c1a2193fdf
SG <sgng1@e.ntu.edu.sg>
2019-09-12 04:35:11 +0100

    edits to minimal node version of emojiReplacer

--- 2Files/emojiReplacer/minimalEmojiReplacer.js
+++ 2Files/emojiReplacer/minimalEmojiReplacer.js
@@ -1,66 +1,40 @@
 // This function uses the Node.js 10.x runtime
 const fs = require("fs");
 
-exports.handler = async event => {
-  const indexPage = fs.readFileSync("./index.html", "utf8");
-
-  if (event.httpMethod === "GET") {
-    return {
-      statusCode: 200,
-      headers: {
-        "Content-Type": "text/html"
-      },
-      body: indexPage
-    };
-  }
-
-  function throwError(errorName) {
-    throw new Error(errorName);
-  }
-  const trimAndStringify = input => input.toString().trim();
-
-  if (event.httpMethod === "POST") {
-    const parsedBodyContent = JSON.parse(event.body);
-    const activityVars = parsedBodyContent["shown"]["0"];
-    const userSolution = parsedBodyContent["editable"]["0"];
-    let executeUserSolution = "";
-    let activityVariables = "";
-    let errorMessage = "";
-    let results = "";
-    try {
-      activityVariables = activityVars.split(",");
-      activityVariables[0]
-        ? activityVariables[1]
-          ? activityVariables[2]
-            ? null
-            : throwError("no third activity variable")
-          : throwError("no second activity variable")
-        : throwError("no first activity variable");
-    } catch (error) {
-      errorMessage = error.message;
+exports.handler = async (event) => {
+    
+    const indexPage= fs.readFileSync("./index.html", "utf8")
+    
+    if (event.httpMethod === 'GET'){
+        return {
+            "statusCode": 200,
+            "headers": {
+            'Content-Type': 'text/html',
+            },
+            "body": indexPage
+        }
     }
-    try {
-      executeUserSolution = eval(userSolution);
-    } catch (error) {
-      errorMessage = `error executing your code: ${error.message}`;
+    
+    const trimAndStr = input => input.toString().trim()
+    
+    if (event.httpMethod === 'POST'){
+        const parsedBodyContent = JSON.parse(event.body)
+        let activityVars = parsedBodyContent["shown"]["0"]
+        const userSolution = parsedBodyContent["editable"]["0"]
+        let results = ""
+        
+        activityVars = activityVars.split(",")
+        const expectedSolution = `"${trimAndStr(activityVars[0])}".replace("${trimAndStr(activityVars[1])}","${trimAndStr(activityVars[2])}")`
+        userSolution ===  expectedSolution ? (results = "You got the answer!") : results = "You have missed the answer."
+        
+        return {
+            "statusCode": 200,
+            "headers": {
+            'Content-Type': 'text/html',
+            },
+            "body": JSON.stringify({
+                "textFeedback": results
+            })
+        }
     }
-    if (!errorMessage) {
-      executeUserSolution ===
-      trimAndStringify(activityVariables[0]).replace(
-        trimAndStringify(activityVariables[1]),
-        trimAndStringify(activityVariables[2])
-      )
-        ? (results = "You got the answer!")
-        : (results = "You have missed the answer... check your code again?");
-    }
-    return {
-      statusCode: 200,
-      headers: {
-        "Content-Type": "text/html"
-      },
-      body: JSON.stringify({
-        textFeedback: results || errorMessage
-      })
-    };
-  }
 };
